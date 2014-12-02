716bc4465c646bdc538105c7da8c57a374c7b862
hw20686832 <stef-hw@163.com>
2014-12-02 17:41:51 +0800

    up

--- scheduler/sync.py
+++ scheduler/sync.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
-#coding:utf-8
-import json
-
+# coding:utf-8
 import redis
 from torndb import Connection
 
@@ -9,24 +7,24 @@ from models import Domain, Flow, Rule
 import settings
 
 rd = redis.Redis(settings.REDIS_HOST, settings.REDIS_PORT)
-db = Connection('192.168.2.228', 'spider', user='crawler', password='crawler')
+db = Connection('127.0.0.1', 'spider', user='spider', password='huangwei')
 
 domains = db.query("select * from domain")
 
 for d in domains:
     domain = Domain(rd, d["domain"])
     domain.update(d)
-    if not "scheduled" in domain:
+    if "scheduled" not in domain:
         domain["scheduled"] = 0
 
     rule_datas = db.query("select * from rule where domain = %s", d["domain"])
     for rule_data in rule_datas:
         rule = Rule(rd, d["domain"])
         rule.update(rule_data)
-    
+
     flows = db.query("select * from flow where domain = %s", d["domain"])
     for f in flows:
         flow = Flow(rd, f["flow"])
         flow.update(f)
-        if not "scheduled" in flow:
+        if "scheduled" not in flow:
             flow["scheduled"] = 0
